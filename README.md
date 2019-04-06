331bf4c78aa27dd67a9c85786051f8da414d504f
jiyinyiyong <jiyinyiyong@gmail.com>
2019-04-06 15:35:16 +0800

    release 0.1.0

--- README.md
+++ README.md
@@ -2,12 +2,27 @@
 Clojars Outdated
 ----
 
-> CLI tool for checking outdated Clojars dependencies.
+> CLI tool for checking outdated Clojars dependencies from `shadow-cljs.edn`.
 
 ### Usage
 
+![](https://img.shields.io/npm/v/@mvc-works/clojars-outdated.svg?label=clojars-outdated)
+
 ```bash
-# TODO
+$ yarn global add @mvc-works/clojars-outdated@0.1.0-a4
+$ clojars-outdated
+Reading shadow-cljs.edn
+Processing 13 tasks: 1 2 3 4 5 6 7 8 9 10 11 12 13
+
+These packages are up to date: mvc-works/hsl mvc-works/shell-page cumulo/recollect cumulo/reel cumulo/util respo/ui respo/feather respo/message cirru/bisection-key
+
+Outdated packages:
+mvc-works/ws-edn   0.1.2 -> 0.1.3
+respo              0.10.2 -> 0.10.9
+respo/alerts       0.3.10 -> 0.3.11
+
+Not able to check: org.clojure/core.incubator
+$
 ```
 
 ### License
--- src/app/main.cljs
+++ src/app/main.cljs
@@ -1,12 +1,89 @@
 
-(ns app.main (:require [skir.client :as skir-client]))
+(ns app.main
+  (:require ["axios" :as axios]
+            ["fs" :as fs]
+            [cljs.reader :refer [read-string]]
+            [clojure.core.async :refer [go chan >! <!]]
+            ["chalk" :as chalk]
+            [clojure.string :as string])
+  (:require-macros [clojure.core.strint :refer [<<]]))
 
-(defn get-page! []
-  (println "Start task...")
-  (skir-client/fetch!
-   "http://news.ycombinator.com/item?id=17533341"
-   (fn [response] (println response))))
+(defn chan-check-dep [[dep-name version]]
+  (let [<result (chan)]
+    (-> axios
+        (.get (str "https://clojars.org/api/artifacts/" dep-name) (clj->js {}))
+        (.then
+         (fn [response]
+           (let [latest-version (.-latest_version (.-data response))]
+             (go (>! <result {:ok? true, :params [dep-name version], :data latest-version})))))
+        (.catch
+         (fn [error]
+           (go (>! <result {:ok? false, :params [dep-name version], :error error})))))
+    <result))
 
-(defn main! [] (println "Started.") (get-page!))
+(defn pad-right [x n] (if (>= (count x) n) x (recur (str x " ") n)))
 
-(defn reload! [] (.clear js/console) (println "Reloaded.") (get-page!))
+(defn display-results! [results]
+  (let [ok-checks (->> results
+                       (filter (fn [check] (:ok? check)))
+                       (map
+                        (fn [check]
+                          {:name (first (:params check)),
+                           :current (last (:params check)),
+                           :latest (:data check)})))
+        failed-checks (->> results (filter (fn [check] (not (:ok? check)))) (map :params))
+        old-packages (->> ok-checks
+                          (filter (fn [info] (not= (:current info) (:latest info)))))
+        latest-packages (->> ok-checks
+                             (filter (fn [info] (= (:current info) (:latest info))))
+                             (map :name)
+                             (map str))]
+    (when (not-empty latest-packages)
+      (println)
+      (println
+       (.gray chalk "These packages are up to date:")
+       (.gray chalk (->> latest-packages (string/join " ")))))
+    (when (not-empty old-packages)
+      (println)
+      (println (.yellow chalk "Outdated packages:"))
+      (let [max-name-length (->> old-packages (map :name) (map str) (map count) (apply max))]
+        (doseq [info old-packages]
+          (println
+           (pad-right (str (:name info)) (+ 2 max-name-length))
+           (:current info)
+           (.gray chalk "->")
+           (:latest info)))))
+    (when (not-empty failed-checks)
+      (println)
+      (println "Not able to check:" (->> failed-checks (map first) (string/join " "))))))
+
+(defn write [& xs] (.apply js/process.stdout.write js/process.stdout (clj->js xs)))
+
+(defn map-chans [chan-f xs]
+  (let [all-tasks (doall (map chan-f xs)), <result (chan), *counter (atom 0)]
+    (go
+     (write (.gray chalk (<< "Processing ~(count all-tasks) tasks: ")))
+     (loop [acc [], tasks all-tasks]
+       (if (empty? tasks)
+         (do (println) (>! <result acc))
+         (do
+          (swap! *counter inc)
+          (write (.gray chalk (<< "~{@*counter} ")))
+          (recur (conj acc (<! (first tasks))) (rest tasks))))))
+    <result))
+
+(defn task! []
+  (println (.gray chalk "Reading shadow-cljs.edn"))
+  (when-not (fs/existsSync "shadow-cljs.edn")
+    (println (.red chalk "Not found"))
+    (.exit js/process 1))
+  (go
+   (let [content (fs/readFileSync "shadow-cljs.edn" "utf8")
+         data (read-string content)
+         <check-results (map-chans chan-check-dep (:dependencies data))
+         results (<! <check-results)]
+     (display-results! results))))
+
+(defn main! [] (task!))
+
+(defn reload! [] (.clear js/console) (println "Reloaded.") (task!))
